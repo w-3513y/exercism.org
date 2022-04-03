b45f71112a3d272e568578ef3c7c3b986a014843
Vanessa <gitrub2021@gmail.com>
2022-04-03 19:05:06 -0300

    included rabbitmq project

---
+++ CodeChallenge/Exercism.Org/exercices/SavingsAccount.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Exercism.Org.exercices
+{
+    static class SavingsAccount
+    {
+        public static float InterestRate(decimal balance) => balance switch
+        {
+            < 0 => 3.213f,
+            < 1000 => 0.5f,
+            < 5000 => 1.621f,
+            _ => 2.475f
+        };
+
+        public static decimal Interest(decimal balance) => balance * (decimal)InterestRate(balance) / 100m;
+
+        public static decimal AnnualBalanceUpdate(decimal balance) => balance + Interest(balance);
+
+        public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
+        {
+            int years = 0;
+            for(var actualBalance = balance; actualBalance < targetBalance; actualBalance = AnnualBalanceUpdate(actualBalance))
+            {
+                years++;
+            }
+            return years;
+        }
+    }
+}
+
